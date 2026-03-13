8d3064ee2993a8bb4dd58a8be85b281dfd436b50
takada291 <tkp291@gmail.com>
2026-03-13 15:18:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'timber-measur-byAI_v2.5.0'; // 
+const CACHE_NAME = 'timber-measur-byAI_v2.6.0'; // 
 const urlsToCache = [
   './',
   'index.html',
@@ -39,6 +39,7 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
 
 
 
